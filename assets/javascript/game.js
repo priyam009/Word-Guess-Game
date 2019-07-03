e7d408830b6e945bf2725ab2ba32b399cc261a38
Priyam <priyam009@live.com>
2019-07-03 21:41:28 +1000

    Adding comments to code and refining code with better solution

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
+//Array of City names
 var wordArr = ["Melbourne", "Sydney", "Mumbai", "Bejing", "Frankfurt", "Tokyo", "Singapore", "London", "Paris", "Barcelona","Seoul","Istanbul", "Dubai", "Bangkok", "Macau", "Rome", "Phuket", "Miami", "Prague", "Milan", "Moscow", "Vienna", "Amsterdam", "Venice", "Johannesburg", "Orlando", "Berlin", "Budapest", "Warsaw", "Brussels", "Munich", "Toronto", "Honololu", "Hanoi", "Zurich", "Jakarta", "Manila", "Marrakech", "Vancouver", "Sharjah", "Athens", "Seattle", "Washington", "Chicago", "Philadelphia", "Atlanta", "Montreal", "Stockholm", "Copenhagen", "Wellington"];
 
-// console.log(wordArr.length);
 
 
+//Variable declaration
 var hiddenWord;
-var wins= 0;
-var losses= 0;
 var gussesLeft;
 var wrongGuess;
-var foundCount;
+var wins= 0;
+var losses= 0;
 
 
 var wordGuessText = document.getElementById("wordguess-text");
@@ -17,53 +17,51 @@ var gussesLeftText = document.getElementById("gussesleft-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 
+//Picking a random Word from the Array
 function randomPick() {
   return wordArr[Math.floor(Math.random() * wordArr.length)].toUpperCase().split('');
 }
 
+//Converting the Word from Array into '_' and saving it in hiddenWord variable
 function hiddenText(word) {
   for(w in word) { 
     hiddenWord.push('_');
   }
 }
 
+//Joins the hiddenWord variable with a space in between and displays it on the screen
 function textDisplay() {
   var displayAnswer = hiddenWord;
   return displayAnswer.join(" ");
 }
 
+//Initializes the game and is called when one game round ends
 function initializeGame() {
-  hiddenWord = [];
-  gussesLeft= 10;
-  wrongGuess=[];
-  foundCount = 0;
+  hiddenWord = [];            //Adding values to the variable declared above
+  gussesLeft= 10;             //Adding values to the variable declared above
+  wrongGuess=[];              //Adding values to the variable declared above
 
-  word = randomPick();
+  word = randomPick();        
   hiddenText(word);
   wordGuessText.textContent = textDisplay();
-
 }
 
 
-
+//Takes input letter from the User and compares it with original word to check its existance
 function compareGuess() {
   document.onkeyup = function(event) {
     var userGuess = event.key.toUpperCase();
     console.log("userGuess",userGuess);
         
-    var count = 0;
   
     for(w in word) {
       if(userGuess == word[w]) {
         hiddenWord[w] = userGuess;
-        count++;
       }
     }
         
-    if(count > 0) {
+    if(hiddenWord.indexOf(userGuess) != -1) {
       wordGuessText.textContent = textDisplay();
-      foundCount += count;
-      console.log("foundCount", foundCount)
     }
     else {
       gussesLeft--;
@@ -71,12 +69,14 @@ function compareGuess() {
       console.log("gussesLeft",gussesLeft);
       console.log("wrongGuess",wrongGuess);
     }
-    roundDecider();
+
+    roundDecider();                      //decides Win or Loss
   };
 }
 
+//Decides Win or Loss and reinitiates the game again
 function roundDecider() {
-  if(foundCount == word.length) {
+  if(hiddenWord.indexOf('_') === -1) {
     wins++;
     console.log("wins",wins);
     initializeGame();
@@ -90,10 +90,5 @@ function roundDecider() {
 }
 
 
-    // var word = randomPick();
-    // console.log("word",word);
-    // console.log("wordlength", word.length);
-    // hiddenText(word);
-    compareGuess();
-
-    initializeGame();
+compareGuess();
+initializeGame();
